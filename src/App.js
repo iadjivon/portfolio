05f3825d423a5ba74ab00e7928749a5057f52fb6
Ida Adjivon <65119712+iadjivon@users.noreply.github.com>
2020-12-30 18:44:57 -0500

    final edits

--- src/App.js
+++ src/App.js
@@ -3,17 +3,107 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="top-div-border">
-        <div className="top-div">
-        </div>
-      </div>
-        <div className="bottom-div">
 
-        </div>
-      
-     
-    </div>
+
+
+ <div className="App">
+
+
+      <div className="top-div-border">
+        <div className="top-div"></div>
+      </div>
+
+      <div> 
+        <div >
+          <header>
+            <ul>
+              {/* <li>
+             <button>  <a href="#"> Home</a> </button>
+             </li>
+            
+              <li>
+              <button> <a href="#"> Contact</a> </button>
+              </li> */}
+            </ul>
+          </header>
+          <body>
+            <div className="introduction">
+              <h1> Hi, I'm Ida.</h1>
+            <p> I am a software engineer with a background in Public Health and program management. I have experience working in non-profit operations, managing projects, and supporting individuals and teams pursue and reach their goals. I am most excited about problem solving and creating applications and software that are well designed and built, user friendly, interactive and fun to use.  
+          </p>
+          {/* <hr/> */}
+
+           <p> JavaScript(ES6) | HTML5 | CSS3 | Node.js | Bootstrap | Git | JQuery | D3.js | GitHub | Express | Node.js | MongoDB | VS-Code
+          </p>
+            </div>
+
+            <div className="getintouch">
+
+              <ul>
+              <li>
+              <button> <a href="#"> LinkedIn</a> </button>
+              </li>
+              
+              <li>
+              <button> <a href="#"> Github</a> </button>
+              </li>
+              </ul>
+             
+            </div>
+           
+            </body>
+            </div>
+
+            <h1 className="he"> SELECTED PROJECTS </h1>
+
+
+            <div className="projects">
+             
+
+              <div className="project-items"> 
+                <div>
+                  <h1> Project 1</h1>
+                  <img src="https://files.constantcontact.com/f2c04391601/33f56ebc-eccd-4013-862e-951e36c5da09.png" /> 
+                  <button> Github </button>
+                  <button> Live App </button>
+                </div>
+
+                <div>
+                  <h1> Project 2</h1>
+                  <img src="https://files.constantcontact.com/f2c04391601/f36c9ca0-e626-46b1-bb61-04c5fe411246.png" /> 
+                  <button> Github </button>
+                  <button> Live App </button>
+                </div>
+
+                <div>
+                  <h1> Project 4</h1>
+                  <img src="https://files.constantcontact.com/f2c04391601/cad6b475-6dc5-477b-80bf-57d7d91fedbb.png" /> 
+                  <button> Github </button>
+                  <button> Live App </button>
+                </div>
+
+                <div>
+                  <h1> Project 5</h1>
+                  <img src="https://files.constantcontact.com/f2c04391601/9a2e5937-4f00-4661-8b11-f4a26e0e4530.png" /> 
+                  <button> Github </button>
+                  <button> Live App </button>
+                </div>
+              </div>
+
+              <div>
+
+              </div>
+
+            </div>
+            <footer className="footer">
+              <h1> Contact Information </h1>
+              <h3> Name: Ida Adjivon </h3>
+              <h3> Email: iadjivon@gmail.com </h3>
+
+            </footer>
+           
+          </div>
+</div>
   );
 }
 
